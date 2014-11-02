14729d5771422f2f244e4890e5f7365fb3a1a808
cponeill <caseyoneill78@hotmail.com>
2014-11-02 10:19:17 -0800

    minor change

--- app.js
+++ app.js
@@ -31,7 +31,6 @@ if ('development' == app.get('env')) {
 app.get('/', routes.index);
 app.get('/users', user.list);
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
-
